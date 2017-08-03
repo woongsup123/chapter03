79586ecba38414315e1626b8769245bff7356c33
bit-user <bit-user@DESKTOP-CNKQU76>
2017-08-03 11:25:11 +0900

    Handle brackets // Need to implement error handling

--- src/calcApp/CalcApp.java
+++ src/calcApp/CalcApp.java
@@ -14,36 +14,46 @@ public class CalcApp {
 			
 			input = concatenate(input);
 			
-			List<Arithmetic> arith = new ArrayList<Arithmetic>();
-			
-			for (int i = 0; i < input.length(); i++) {
-				
-				switch (input.charAt(i)){
-				case '+':
-					arith.add(new Add());
-					break;
-				case '-':
-					arith.add(new Sub());
-					break;
-				case '*':
-					arith.add(new Mul());
-					break;
-				case '/':
-					arith.add(new Div());
-					break;
-				default:
-					break;
-				}
-			}
-			
-			List<String> numsStr = Arrays.asList(input.split("[\\+\\-\\*\\/]"));
+			List<String> numsStr = Arrays.asList(input.split("[\\+\\-\\*\\/\\(\\)]"));
 			List<Double> nums = new ArrayList<Double>();
 			
 			for (int i = 0; i < numsStr.size(); i++) {
-				nums.add(Double.parseDouble(numsStr.get(i)));
+				
+				String s = numsStr.get(i);
+				
+				if (isNumeric(s)) {
+					nums.add(Double.parseDouble(s));
+				}
 			}
 			
-			System.out.println(calculate(arith, nums));
+			List<Arithmetic> arith = new ArrayList<Arithmetic>();
+			int openBracketPos = 0;
+			int closeBracketPos = 0;
+			for (int i = 0; i < input.length(); i++) {
+				
+				char c = input.charAt(i);
+				if (c == '+') {
+					arith.add(new Add());
+				}
+				else if ( c == '-' ) {
+					arith.add(new Sub());
+				}
+				else if ( c == '*' ) {
+					arith.add(new Mul());
+				}
+				else if ( c == '/' ) {
+					arith.add(new Div());
+				}
+				else if ( c == '(' ) {
+					openBracketPos = arith.size();
+				}
+				else if ( c == ')' ) {
+					closeBracketPos = arith.size();
+					calculate(arith, nums, openBracketPos, closeBracketPos);
+				}
+			}
+			
+			System.out.println(calculate(arith, nums, 0, arith.size()));
 		}
 	}
 	
@@ -57,26 +67,42 @@ public class CalcApp {
 		return sum;
 	}
 	
-	private static Double calculate(List<Arithmetic> arith, List<Double> nums) {
+	private static Double calculate(List<Arithmetic> arith, List<Double> nums,
+									int init, int end) { //from initial sign index to final sign index
 		
-		
-		for (int i = 0; i < arith.size(); i++) {
+		for (int i = init; i < end; i++) {
 			if (arith.get(i) instanceof Mul || arith.get(i) instanceof Div) {
 				double update = arith.get(i).calculate(nums.get(i), nums.get(i+1));
 				nums.set(i, update);
 				nums.remove(i+1);
 				arith.remove(i);
+				end--;
 				i--;
 			}
 		}
-		for (int i = 0; i < arith.size(); i++) {
+		
+		for (int i = init; i < end; i++) {
 			double update = arith.get(i).calculate(nums.get(i), nums.get(i+1));
 			nums.set(i, update);
 			nums.remove(i+1);
 			arith.remove(i);
+			end--;
 			i--;
 		}
 		
 		return nums.get(0);
+	}
+	
+	public static boolean isNumeric(String str)  
+	{  
+		try
+		{
+			double d = Double.parseDouble(str);  
+		}
+		catch(NumberFormatException nfe)
+		{
+			return false;
+		}
+		return true;  
 	}
 }
